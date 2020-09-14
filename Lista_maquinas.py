0f7b01578409085c9d8dbf93d32c013981abb488
th3clu5t3r <contato@nathanqsilva.com.br>
2020-09-14 19:52:03 -0300

    adicionado variaveis solicitando a senha para se conectar a api

--- Lista_maquinas.py
+++ Lista_maquinas.py
@@ -1,7 +1,11 @@
 from pyzabbix.api import ZabbixAPI
 
+# Solicita senha e usuário utilizados na api
+UserName = input("Digite o nome de usário: ")
+Password = input("Digite a sua senha: ")
+
 # Informaçoes sobre o login no servidor zabbix
-zapi = ZabbixAPI(url='http://15.228.60.226/', user='nathan.silva', password='S3c77$1Nfr488')
+zapi = ZabbixAPI(url='http://15.228.60.226/', user=UserName, password=Password)
 
 # Pega todos os hosts monitorados
 result1 = zapi.host.get(monitored_hosts=1, output='extend')
@@ -16,3 +20,6 @@ result2 = zapi.do_request('host.get',
 # Filtra resultados
 hostnames1 = [host['host'] for host in result1]
 hostnames2 = [host['host'] for host in result2['result']]
+
+print(hostnames1)
+print(hostnames2)
