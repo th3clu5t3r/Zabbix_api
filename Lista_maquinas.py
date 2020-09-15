8929cd04169d6eb5b313522b769e8bfd7946f549
th3clu5t3r <contato@nathanqsilva.com.br>
2020-09-14 21:02:05 -0300

    Alterado para coletar grupos cadastrados no zabbix

--- Lista_maquinas.py
+++ Lista_maquinas.py
@@ -1,25 +1,17 @@
-from pyzabbix.api import ZabbixAPI
+from zabbix_api import ZabbixAPI
 
-# Solicita senha e usuário utilizados na api
+server = "http://15.228.60.226/"
 UserName = input("Digite o nome de usário: ")
 Password = input("Digite a sua senha: ")
 
-# Informaçoes sobre o login no servidor zabbix
-zapi = ZabbixAPI(url='http://15.228.60.226/', user=UserName, password=Password)
 
-# Pega todos os hosts monitorados
-result1 = zapi.host.get(monitored_hosts=1, output='extend')
+# Instanciando a API
+zapi = ZabbixAPI(server = server, path = "", log_level = 0)
+zapi.login(UserName, Password)
 
-# Pega todos os hosts desabilitados
-result2 = zapi.do_request('host.get',
-                            {
-                                'filter': {'status': 1},
-                                'output': 'extend'
-                            })
-
-# Filtra resultados
-hostnames1 = [host['host'] for host in result1]
-hostnames2 = [host['host'] for host in result2['result']]
-
-print(hostnames1)
-print(hostnames2)
+# Obtendo uma lista com os grupos que ja estão no zabbix
+grupos = zapi.hostgroup.get({"output":"extend"})
+for q in grupos:
+    GoupID = q[u'groupid']
+    GroupName = q[u'name']
+    print ("Grupo: %s " %(GroupName))
